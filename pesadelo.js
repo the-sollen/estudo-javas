16992c0868dce8f84dca4d57cff50013df5767bf
agner <agner.flucas@.com>
2023-11-30 11:57:26 -0300

    estudo javas

---
+++ pesadelo.js
@@ -0,0 +1,5 @@
+const person = {firstname:"johon", lastname:"doe" , age:"57"};
+const person2 = ["kelly" , "silva", 26];
+ 
+document.getElementById("johon").innerHTML = person.firstname +" "+person.lastname +" "+ person.age;
+document.getElementById("kelly").innerHTML = person2[0] + " " + person2[1] + " " +  person2[2] + " ";
---
+++ testa.js
@@ -0,0 +1,5 @@
+const cars = [];
+cars [0]= "saab";
+cars [1]= "volvo";
+cars [2]=  "BMW";
+document.getElementByid("demo").innerHTML = cars;
