cf83e4720eda07913d408e449a351135814b1c2c
agner <agner.flucas@.com>
2023-12-06 10:41:52 -0300

    estudo java

---
+++ Untitled-2.js
@@ -0,0 +1,11 @@
+document.getElementById("botao1").addEventListener("click", function() {
+     alert("Clicou no Botão 1");
+   });
+
+   document.getElementById("botao2").addEventListener("click", function() {
+     alert("Clicou no Botão 2");
+   });
+
+   document.getElementById("botao3").addEventListener("click", function() {
+     alert("Clicou no Botão 3");
+   });
